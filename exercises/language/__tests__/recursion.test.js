877983f99249d8d43da458c170f932f764a055f4
Dustin Wheeler <dustin.wheeler@nutanix.com>
2022-02-03 09:51:28 -0500

    Shuffle test around for legibility

--- exercises/language/__tests__/recursion.test.js
+++ exercises/language/__tests__/recursion.test.js
@@ -1,5 +1,11 @@
 const { getLeafNodes } = require('../recursion')
 
+test('return all leaves from the tree of awesome', () => {
+  const leaves = getLeafNodes(treeOfAwesome)
+
+  expect(leaves.sort()).toEqual(['B', 'C', 'E', 'G', 'I', 'J', 'K'])
+})
+
 const treeOfAwesome = {
   name: 'Root',
   children: [
@@ -53,9 +59,3 @@ const treeOfAwesome = {
     },
   ]
 }
-
-test('return all leaves from the tree of awesome', () => {
-  const leaves = getLeafNodes(treeOfAwesome)
-
-  expect(leaves.sort()).toEqual(['B', 'C', 'E', 'G', 'I', 'J', 'K'])
-})
